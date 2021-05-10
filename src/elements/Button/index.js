0e0679e40f884999ca6d7987be8cad2deaa7283c
nanandt <rizkyfatih007@gmail.com>
2021-05-11 00:22:30 +0700

    first commit

---
+++ src/elements/Button/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import propTypes from "prop-types";
+
+export default function Button(props) {
+  return <div></div>;
+}
+
+Button.propTypes = {
+  type: propTypes.oneOf(["Button", "Link"]),
+  onClick: propTypes.func,
+  target: propTypes.string,
+  className: propTypes.string,
+  isDisabled: propTypes.bool,
+  isLoading: propTypes.bool,
+  isSmall: propTypes.bool,
+  isLarge: propTypes.bool,
+  isBlock: propTypes.bool,
+  isShadow: propTypes.bool,
+};
